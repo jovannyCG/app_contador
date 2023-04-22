7cb7d07577819eb8eb9551c17b3f823e41a6cd06
Jovanny Carlos Gonzalez <jovannycarlosgonzalez9@gmail.com>
2023-04-22 12:09:31 -0600

    cambio de estado de app

--- lib/presentation/screens/couters/conter_screen.dart
+++ lib/presentation/screens/couters/conter_screen.dart
@@ -1,25 +1,36 @@
 import 'package:flutter/material.dart';
 
-class CouterScreen extends StatelessWidget {
+class CouterScreen extends StatefulWidget {
+
   const CouterScreen({super.key});
 
+  @override
+  State<CouterScreen> createState() => _CouterScreenState();
+}
+
+class _CouterScreenState extends State<CouterScreen> {
+  int clickCouter =0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: const Text('counter Screen'),
-        actions: [
-
-        ]),
+        title: const Text('counter Screen'),),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){},
+        onPressed: (){
+          clickCouter ++;
+          setState(() {
+           clickCouter ++;
+          });
+        },
         child: const Icon(Icons.plus_one),
         ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const [Text('10', style: TextStyle(fontSize: 130, fontWeight: FontWeight.w100),), Text('cantidad de clics', style: TextStyle(fontSize: 30, ),)],
+          children:  [
+            Text('$clickCouter', style: const TextStyle(fontSize: 130, fontWeight: FontWeight.w100),), 
+            const Text('cantidad de clics', style: TextStyle(fontSize: 30, ),)],
         ),
       ),
     );
