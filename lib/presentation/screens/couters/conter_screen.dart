38f141cad039dc3e5ad8a13e443612055660f822
Jovanny Carlos Gonzalez <jovannycarlosgonzalez9@gmail.com>
2023-04-22 12:15:40 -0600

    cambio de estado de textos

--- lib/presentation/screens/couters/conter_screen.dart
+++ lib/presentation/screens/couters/conter_screen.dart
@@ -20,7 +20,6 @@ class _CouterScreenState extends State<CouterScreen> {
         onPressed: (){
           clickCouter ++;
           setState(() {
-           clickCouter ++;
           });
         },
         child: const Icon(Icons.plus_one),
@@ -30,7 +29,8 @@ class _CouterScreenState extends State<CouterScreen> {
           mainAxisAlignment: MainAxisAlignment.center,
           children:  [
             Text('$clickCouter', style: const TextStyle(fontSize: 130, fontWeight: FontWeight.w100),), 
-            const Text('cantidad de clics', style: TextStyle(fontSize: 30, ),)],
+            (clickCouter==1) ? const Text('clic', style: TextStyle(fontSize: 30, ),)
+             : const Text(' clics', style: TextStyle(fontSize: 30, ),)],
         ),
       ),
     );
